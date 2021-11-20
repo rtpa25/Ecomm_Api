c53a1c38fe6fd601c14e6f089a3c936155023f2e
Ronit Panda <pandaronit25@gmail.com>
2021-11-20 08:41:00 +0530

    fixed the jwt premature expiry bug

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -4,7 +4,7 @@ const cookieToken = (user, res) => {
   const token = user.getJwtToken();
 
   const options = {
-    // expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
     httpOnly: true,
   };
 
